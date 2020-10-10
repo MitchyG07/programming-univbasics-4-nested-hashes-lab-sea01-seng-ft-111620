e8c230c15585468822acb3e0c384d36fe086d18e
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-10 01:44:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -54,8 +54,10 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-  return programmer_hash[:alan_kay][:known_for]
+  puts programmer_hash[:alan_kay][:known_for]
 end
+
+dennis_ritchies_language
 
 def adding_matz
 # add the following information to the top level of programmer_hash
